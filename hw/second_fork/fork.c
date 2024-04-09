43f456af99152c02fcf7a5846b33660ee53ad5f2
13urdock <uyeong7181@gmail.com>
2024-04-09 22:10:31 +0900

    fork child process before loop

---
+++ hw/second_fork/fork.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <wait.h>
+#include <unistd.h>
+
+int main(){
+	pid_t pid; // process id(int)
+
+	pid = fork(); // create? fork? a child process
+	for (int i = 0; i < 2; i++){
+		if (pid < 0){ // failed to create child process
+			printf("Fork Failed");
+		}
+		else if (pid == 0){ // child process(pid = 0)
+			printf("Hello from child in %d loop\n", i);
+		}
+		else{ // parent process
+			wait(NULL);
+			printf("Hello from parent in %d loop\n", i);
+		}
+		printf("%d loop\n", i);
+	}
+
+	return 0;
+}
+
+
